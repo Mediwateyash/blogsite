464fca24ecc47a21f821a2f2f17bdaf70d5337f2
mediwateyash <diwateyash2004@gmail.com>
2025-02-02 21:31:34 +0530

    post detail button

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id: "1a",
         username : "Yash diwate",
         post : "This is my first post",
     },
     {
+        id: "2b",
         username : "Anush Gajbhiye",
         post : "This is my second post",
     },
     {
+        id: "3c",
         username : "Vaibhav nikumbh",
         post : "This is my third post", 
         },
@@ -41,6 +44,13 @@ posts.push({username, post:content});
     res.redirect("/posts");
 });
 
+app.get('/posts/:id', (req, res) => {
+    let {id}=req.params;
+    console.log(id);
+    let post =posts.find((p)=> id === p.id)
+    console.log(post);
+       res.render("show.ejs", {post});
+    });
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
