3070cdce1e248b20ba068fbea1272672663e9443
mediwateyash <diwateyash2004@gmail.com>
2025-02-03 03:04:08 +0530

    edit post working

--- index.js
+++ index.js
@@ -1,67 +1,75 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 const path = require('path');
-const {v4: uuidv4} = require('uuid');
-
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-
-app.set ("view engine", "ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
         id: uuidv4(),
-        username : "Yash diwate",
-        post : "This is my first post",
+        username: "Yash diwate",
+        post: "This is my first post",
     },
     {
         id: uuidv4(),
-        username : "Anush Gajbhiye",
-        post : "This is my second post",
+        username: "Anush Gajbhiye",
+        post: "This is my second post",
     },
     {
         id: uuidv4(),
-        username : "Vaibhav nikumbh",
-        post : "This is my third post", 
-        },
+        username: "Vaibhav nikumbh",
+        post: "This is my third post",
+    },
+];
 
-]
-
-app.get ("/posts",(req,res) =>{
+app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
-}
-)
+});
 
-app.get ("/posts/new",(req,res) => {
+app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
-})
+});
 
 app.post('/upload', (req, res) => {
-let {username , content}=req.body;
- let id = uuidv4();
-posts.push({id ,username, post:content}); 
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, post: content });
     res.redirect("/posts");
 });
 
 app.get('/posts/:id', (req, res) => {
     let { id } = req.params;
-    console.log("Requested ID:", id);
-
     let post = posts.find((p) => p.id === id);
-    console.log("Found Post:", post);
-
     res.render("show.ejs", { post });
 });
 
 app.get('/posts/:id/edit', (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => p.id === id);
-    res.render("edit.ejs",{post} );
-})
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    res.render("edit.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newPost = req.body.post;
+    let post = posts.find((p) => p.id === id);
+    if (post) {
+        post.post = newPost;
+        console.log("Updated Post:", post);
+        res.redirect(`/posts/${id}`);  // Redirect to the updated post page
+    } else {
+        res.status(404).send("Post not found");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
