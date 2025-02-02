8ad8673d75045a12c294a61121cbae91046c8e48
mediwateyash <diwateyash2004@gmail.com>
2025-02-02 19:56:49 +0530

    create new post

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000
 const path = require('path');
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+
 
 app.set ("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -20,7 +21,7 @@ let posts = [
     },
     {
         username : "Vaibhav nikumbh",
-        post : "This is my third post",
+        post : "This is my third post", 
         },
 
 ]
@@ -29,6 +30,17 @@ app.get ("/posts",(req,res) =>{
     res.render("index.ejs", { posts });
 }
 )
+
+app.get ("/posts/new",(req,res) => {
+    res.render("new.ejs");
+})
+
+app.post('/upload', (req, res) => {
+let {username , content}=req.body;
+posts.push({username, post:content}); 
+    res.redirect("/posts");
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
